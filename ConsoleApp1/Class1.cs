e8b0d5c0da73f9265043f634b4484c0ba798048b
LeiYangGH <675686066@qq.com>
2022-02-10 22:41:31 +0800

    cannot reproduce

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Class1));
+        public void Foo()
+        {
+            log.Debug($"Foo.{Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(10);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using log4net.Config;
+
+XmlConfigurator.Configure(new FileInfo("log4net.xml"));
+log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+for (int i = 0; i < 20; i++)
+{
+    new Thread(() => new Class1().Foo()).Start();
+}
+log.Logger.Repository.Shutdown();
+Console.WriteLine("end!");
+Console.ReadKey();
